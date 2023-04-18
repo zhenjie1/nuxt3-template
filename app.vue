a2077fff4a8b7501107c6635fc8ff39a583c2ba3
zhenjie <1304119519@qq.com>
2023-04-18 17:23:34 +0800

    init

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <NuxtLayout>
+    app
+    <!-- <NuxtWelcome /> -->
+  </NuxtLayout>
+</template>
---
+++ pages/hello.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="">
+    content
+  </div>
+</template>
